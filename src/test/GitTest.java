7a5da3221e3d9f5fe0fccd2d1dcc6f5fb899b7f3
Ilian Tomalyanov <i.tomalyanov@gmail.com>
2013-06-30 18:43:21 +0300

    git test class

---
+++ src/test/GitTest.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package test;
+
+/**
+ * @author viper : Jun 30, 2013
+ *
+ */
+public class GitTest {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Git :) ");
+	}
+}
