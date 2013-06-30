e0295fd76c051aefaced7900b8f9a9a97767408f
Ilian Tomalyanov <i.tomalyanov@gmail.com>
2013-06-30 19:56:31 +0300

    Local Repo test

--- src/test/GitTest.java
+++ src/test/GitTest.java
@@ -12,5 +12,7 @@ public class GitTest {
 	public static void main(String[] args) {
 		System.out.println("Hello Git :) ");
 		System.out.println("Version control is good!");
+		
+		System.out.println("Local repo test");
 	}
 }
